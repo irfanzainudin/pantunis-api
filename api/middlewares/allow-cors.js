c8afcdeab4316c4407072302ba8e2df33400a0e0
irfanzainudin <m.irfan.zain@gmail.com>
2024-01-05 12:37:30 +1100

    wildcard it is

--- api/middlewares/allow-cors.js
+++ api/middlewares/allow-cors.js
@@ -1,7 +1,8 @@
 const allowCors = (fn) => (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "https://www.pantunis.com");
-  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "https://www.pantunis.com");
+  // res.setHeader("Vary", "Origin");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"

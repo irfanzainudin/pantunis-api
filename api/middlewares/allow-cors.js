7ca6e68969ce407b8e49c012815700baa18a8b18
irfanzainudin <m.irfan.zain@gmail.com>
2024-01-05 12:26:52 +1100

    istg if a forward slash is the issue...

--- api/middlewares/allow-cors.js
+++ api/middlewares/allow-cors.js
@@ -1,6 +1,6 @@
 const allowCors = (fn) => (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "https://www.pantunis.com/");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.pantunis.com");
   res.setHeader("Vary", "Origin");
   res.setHeader(
     "Access-Control-Allow-Methods",

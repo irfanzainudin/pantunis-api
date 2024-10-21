edb481a1d3a3b5ab1efc381cb7df0738ee76ce24
irfanzainudin <m.irfan.zain@gmail.com>
2024-10-22 05:40:43 +0800

    added index.html and show it as landing/home page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import cariGunaTema from "./cariGunaTema.js";
 import dermaPantun from "./dermaPantun.js";
 import jumlah from "./jumlah.js";
 
+import path, { dirname } from "path";
+import { fileURLToPath } from 'url';
+import fs from "fs";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,8 +23,14 @@ app.use(express.urlencoded({
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.get('/', (req, res) => {
-    return res.status(200).send(`<h1>Hello World! NODE_ENV is ${NODE_ENV}</h1>`);
+    // return res.status(200).send(`<h1>Hello World! NODE_ENV is ${NODE_ENV}</h1>`);
+    fs.readFile(path.resolve(__dirname, "index.html"), 'utf8', (err, text) => {
+        res.status(200).send(text);
+    });
 });
 
 app.get('/jumlah', jumlah);
